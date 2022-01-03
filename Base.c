34eac780540dfdd85f8d1dbbe860f47b5b902226
JonathanYuri <86935880+JonathanYuri@users.noreply.github.com>
2022-01-03 14:36:50 -0300

    arestas

--- Base.c
+++ Base.c
@@ -10,35 +10,60 @@
 
 int main()
 {
-    int vertice, aresta, n = 1, x, y;
-    scanf ("%d %d", &vertice, &aresta);
-    int vertices[vertice], graus[vertice], arestas[vertice][vertice-1], pesos[vertice][vertice-1];
+    int vert, ares, n = 1, x, y, vert_anterior = 1;
+    scanf ("%d %d", &vert, &ares);
+    int vertices[vert], graus[vert], cont_arestas[vert], arestas[vert][vert-1], pesos[vert][vert-1];
     
     //preencher graus com 0's
-    for (int i = 0; i < vertice; i++)
+    for (int i = 0; i < vert; i++)
     {
         graus[i] = 0;
+        cont_arestas[i] = 0;
     }
     
     //preencher vetor vertices
-    for (int i = 0; i < vertice; i++)
+    for (int i = 0; i < vert; i++)
     {
         vertices[i] = n;
         n++;
         //printf ("%d\n", vertices[i][0]);
     }
+
+    for (int i = 0; i < vert; i++)
+    {
+        for (int j = 0; j < vert - 1; j++)
+        {
+            arestas[i][j] = 0;
+        }
+    }
     
-    //preencher graus de cada vértice
-    for (int i = 0; i < aresta; i++)
+    //preencher graus de cada vértice e a matriz arestas
+    for (int i = 0; i < ares; i++)
     {
         scanf ("%d %d\n", &x, &y);
         //printf ("%d %d\n", x, y);
         graus[x-1] += 1;
         graus[y-1] += 1;
+        
+        arestas[x-1][cont_arestas[x-1]] = y;
+        arestas[y-1][cont_arestas[y-1]] = x;
+        
+        cont_arestas[y-1] += 1;
+        cont_arestas[x-1] += 1;
+    }
+
+    // printar arestas
+    for (int i = 0; i < vert; i++)
+    {
+        for (int j = 0; j < vert - 1; j++)
+        {
+            printf("%i ", arestas[i][j]);
+        }
+        printf ("\n");
     }
     
     //print
-    for (int i = 0; i < vertice; i++)
+    for (int i = 0; i < vert; i++)
     {
         printf ("%d ", graus[i]);
     }
